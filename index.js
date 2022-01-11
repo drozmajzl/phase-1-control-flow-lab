4933d24ed4780d867b07911f6c4bbffd0d8e8c5e
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-01-10 20:05:33 -0500

    commit tree 43df037cb63567364369d9fbc2cbf867abd86492

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let response;
+  if (distance <= 400) { 
+    response = 'This one is on me!';
+    }
+  else if (distance > 2500) {
+    response = 'No can do.';
+  }
+  else if (distance > 2000) {
+    response = 'I will gladly take your thirty bucks.';
+  }
+    return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const driverResponse = (city === 'NYC') ? "Ok, sounds good." : "No go."
+  return driverResponse;
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  // switch statement to return a response based on generosity of tip.
+  let driverResponse;
+  switch (tip) {
+  case 'generous':
+        driverResponse = 'Thank you so much.';
+        break;
+  case 'not as generous':
+        driverResponse = 'Thank you.';
+        break;
+  default:
+        driverResponse = 'Bye.';
+  }
+  return driverResponse;
 }
